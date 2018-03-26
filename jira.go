bdc7861f8acd389ebb2034293445a4d3a99814c5
Valere JEANTET <valere.jeantet@gmail.com>
2018-03-26 18:13:33 +0200

    handle time

--- jira.go
+++ jira.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
+	"strings"
+	"time"
 
 	"github.com/fatih/structs"
 	jira "gopkg.in/andygrunwald/go-jira.v1"
 )
+
+const defaultFields string = "created,updated,duedate,resolutiondate,summary,description,issuetype,reporter,status"
 
 type jiraClient struct {
 	c    *jira.Client
@@ -15,6 +19,7 @@ type jiraClient struct {
 func newJiraClient(url, username, password string, log func(string, ...interface{})) (*jiraClient, error) {
 	var err error
 	j := &jiraClient{}
+	j.Logf = log
 
 	j.c, err = jira.NewClient(nil, url)
 	if err != nil {
@@ -32,18 +37,22 @@ func newJiraClient(url, username, password string, log func(string, ...interface
 }
 
 func (j *jiraClient) CountIssuesByKey(issueID string) (int, error) {
-	_, body, err := j.c.Issue.Get(issueID, nil)
+	fmt.Println(issueID)
+	_, _, err := j.c.Issue.Get(issueID, nil)
+
 	if err != nil {
-		return 0, fmt.Errorf("CountIssuesByKey : %s, %s", err, issueID)
+		j.Logf("CountIssuesByKey : %s, %s", err, issueID)
+		return 0, nil
 	}
 
-	return body.Total, nil
+	return 1, nil
 }
 
 func (j *jiraClient) CountIssuesByJQL(jql string) (int, error) {
 	options := jira.SearchOptions{
 		StartAt:    0,
 		MaxResults: 1,
+		Fields:     []string{"summary"},
 	}
 	_, body, err := j.c.Issue.Search(jql, &options)
 	if err != nil {
@@ -54,12 +63,15 @@ func (j *jiraClient) CountIssuesByJQL(jql string) (int, error) {
 }
 
 func (j *jiraClient) FindOneIssueByKey(issueID string, max int, cfields []string) (chan map[string]interface{}, error) {
-	issue, _, err := j.c.Issue.Get(issueID, nil)
+	issue, _, err := j.c.Issue.Get(issueID, &jira.GetQueryOptions{
+		Fields: defaultFields,
+	})
+
 	if err != nil {
 		return nil, err
 	}
 
-	iChan := make(chan map[string]interface{})
+	iChan := make(chan map[string]interface{}, 1)
 	iChan <- issueToMSI(issue)
 	close(iChan)
 
@@ -67,12 +79,14 @@ func (j *jiraClient) FindOneIssueByKey(issueID string, max int, cfields []string
 }
 
 func (j *jiraClient) FindIssuesByJQL(jql string, max int, cfields []string) (chan map[string]interface{}, error) {
+	dfields := append(strings.Split(defaultFields, ","), cfields...)
 	options := jira.SearchOptions{
 		StartAt:    0,
 		MaxResults: max,
+		Fields:     dfields,
 	}
 
-	iChan := make(chan map[string]interface{})
+	iChan := make(chan map[string]interface{}, max)
 
 	go func() {
 		defer close(iChan)
@@ -91,6 +105,18 @@ func (j *jiraClient) FindIssuesByJQL(jql string, max int, cfields []string) (cha
 
 func issueToMSI(i *jira.Issue) map[string]interface{} {
 	msi := structs.Map(i.Fields)
+	if !time.Time(i.Fields.Created).IsZero() {
+		msi["created"] = time.Time(i.Fields.Created)
+	}
+	if !time.Time(i.Fields.Updated).IsZero() {
+		msi["updated"] = time.Time(i.Fields.Updated)
+	}
+	if !time.Time(i.Fields.Duedate).IsZero() {
+		msi["duedate"] = time.Time(i.Fields.Duedate)
+	}
+	if !time.Time(i.Fields.Resolutiondate).IsZero() {
+		msi["resolutiondate"] = time.Time(i.Fields.Resolutiondate)
+	}
 	return msi
 }
 
